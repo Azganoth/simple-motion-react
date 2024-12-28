840c9c0555ad8c2613ebe667e135ef0cfc74806e
Ademir José Ferreira Júnior <ademirj.ferreirajunior@gmail.com>
2024-12-28 20:37:51 -0300

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+/** @type {import('rollup').InputOptions[]} */
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      { file: "lib/index.js", format: "esm" },
+      { file: "lib/index.cjs", format: "cjs", exports: "auto" },
+    ],
+    plugins: [
+      resolve(),
+      typescript({ declaration: true, declarationDir: "lib/types" }),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "lib/types/index.d.ts",
+    output: {
+      file: "lib/index.d.ts",
+      format: "esm",
+    },
+    plugins: [dts()],
+  },
+];
